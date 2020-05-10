fe0d2db32d47eccdc4390b39b92c7dd3d16a1227
calm4git <35043090+calm4git@users.noreply.github.com>
2020-05-10 16:43:20 +0200

    Starting to reimplement using TFT_eSPI
    change from uCGlib to TFT_eSPI and TFTShapes

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -1,165 +1,85 @@
-//ESP8266 includes
-/*
-#include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
-#include <Hash.h>
-#include <ESPAsyncUDP.h>
-*/
-
-//ESP32 includes
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <esp_wifi.h>
+#include "SPI.h"
+#include "TFT_eSPI.h"
+#include "src/layout1.h"
+#include "src/layout2.h"
+#include "src/layout3.h"
 
 
-#include <SPI.h>
-#include <SPIFFS.h>
-#include <FS.h>
+TFT_eSPI lcd = TFT_eSPI();       // Invoke custom library
 
-
-#include "timecore.h"
-#include "datastore.h"
-
-#include <Wire.h>
-#include "RTClib.h"
-#include <Ticker.h>
-
-#include <TimeLib.h>
-#include "NTP_Client.h"
-
-#include "display.h"
-
-RTC_DS3231 rtc_clock;
-
-Timecore timec;
-NTP_Client NTPC;
-
-Ticker TimeKeeper;
-/* 63 Char max and 17 missign for the mac */
+ClockLayout3 L3 = ClockLayout3(lcd);
+ClockLayout2 L2 = ClockLayout2(lcd);
+ClockLayout1 L1 = ClockLayout1(lcd);
 
 
 
-
-
-uint32_t RTC_ReadUnixTimeStamp(bool* delayed_result){
- DateTime now = rtc_clock.now();
- *delayed_result=false;
- return now.unixtime();
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  lcd.init();
+  lcd.setRotation(1);
+  lcd.fillScreen(TFT_BLACK);
+  /*
+  L2.UpdateTime(2020,5,1,0,8,5,48);
+  L2.ForceScreenRefresh();
+  */
+  L3.UpdateTime(2020,5,1,0,8,5,48);
+  L3.ForceScreenRefresh();
 }
 
-void RTC_WriteUnixTimestamp( uint32_t ts){
-  rtc_clock.adjust(DateTime( ts));
-}
+void loop() {
+  static uint32_t ticks =0;
+  static uint8_t hour = 8;
+  static uint8_t minute = 5;
+  static uint8_t second = 45;
+  static uint16_t Year = 2020;
+  static uint8_t Month = 0;
+  static uint8_t Day=1;
+  static uint8_t DOW = 0;
+  // put your main code here, to run repeatedly:
+  delay(10);
+  second++;
+  if(second >=60){
+    second =0;
+    minute++;
+    if(minute>=60){
+      minute=0;
+      hour++;
+      if(hour>=24){
+        hour=0;
+      }
+    }
+  }
+  ticks++;
+  if(ticks==50){
+    Day++;
+    if(Day>31){
+      Day=1;
+    }
+    DOW++;
+    if(DOW>6){
+      DOW=0;
+    }
+
+  }
+
+  if(ticks==190){
+     Month++;
+     if(Month>11){
+       Month=0;
+     }
+  }
 
 
-
-/*SETUP*/
-void setup()
-{
-  uint8_t connect_cnt=0;
- // We start by connecting to a WiFi network
-  Serial.begin (115200);
-  datastoresetup();
-  SPIFFS.begin();
-  /* we need to wait for a second */
-  Serial.println("Booting...");
-  delay(1000);
-  Serial.println("Init LCD");   
-  InitDisplay();
-  Serial.println("Init WiFi");   
-  initWiFi();
-  TimeKeeper.attach_ms(10, callback);
-  Serial.println("Read Timecore Config");
-  timecoreconf_t cfg = read_timecoreconf();
-  timec.SetConfig(cfg);
-
-  
-  if (! rtc_clock.begin()) {
-    Serial.println("Couldn't find RTC");
-   
+  //increase time and let the display draw
+  L3.UpdateTime(Year,Month,Day,DOW,hour,minute,second);
+  if(ticks<100){
+    L3.UpdateAlarmInfo(false,  2,  9,  6);
   } else {
-    DateTime now = rtc_clock.now();
-      
-    rtc_source_t I2C_DS3231;
-
-    I2C_DS3231.SecondTick=NULL;
-    I2C_DS3231.type = RTC_CLOCK;
-    I2C_DS3231.ReadTime=RTC_ReadUnixTimeStamp;
-    I2C_DS3231.WriteTime=RTC_WriteUnixTimestamp;
-    timec.RegisterTimeSource(I2C_DS3231);
-    
-      
-    timec.SetUTC(now.unixtime()  , RTC_CLOCK );
-  
-  
+    L3.UpdateAlarmInfo(true,  2,  9,  6);
+    if(ticks>200){
+      ticks=0;
+    }
   }
 
-  Serial.println("Setup NTP now");
-  NTPC.ReadSettings();
-  NTPC.begin( &timec );
-  NTPC.Sync();
-  SetupDisplay();
 }
-
-void NTPC_Tick( ){
- 
-}
-
-/* we need to presace it to 1hz for the button i guesst ....*/
-/* We use 100Hz */
-
-void callback()
-{
-  static uint8_t prescaler_1s=0;
-  prescaler_1s++;
-  if(prescaler_1s>=100){
-    prescaler_1s=0;
-    /*Increment time */ 
-   
-    //Serial.println("+");
-    timec.RTC_Tick();
-    NTPC.Tick();
-     
-  }
-  
-  /* this dose timekeeping */
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*==================================================*/
-/*                 MAIN LOOP                        */
-/*==================================================*/
-void loop()
-{
-NetworkTask();
-DisplayTask();
-  
-  // ================= END OF MAIN LOOP ======================
-}
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-
---
+++ Firmware/src/layout1.h
@@ -0,0 +1,40 @@
+ #ifndef __LAYOUT1_H__
+ #define __LAYOUT1_H__
+
+#include "TFT_eSPI.h"
+
+ class ClockLayout1{
+
+    public:
+        
+        ClockLayout1( TFT_eSPI& lcd );
+
+        void UpdateTime( uint16_t year, uint8_t month, uint8_t day, uint8_t dow, uint8_t hour, uint8_t minute, uint8_t second );
+        void UpdateAlarmInfo(bool Enabled, uint8_t dow, uint8_t hour, uint8_t minute);
+        void ForceScreenRefresh( void );
+
+    private:
+        
+        void DrawAlarmONOFF( bool ON );
+        void DrawAlarmNEXT( uint8_t hour, uint8_t minute, uint8_t dow, bool clear );
+        void DrawDate( uint16_t year, uint8_t month, uint8_t day, uint8_t dow );
+
+
+        int8_t second_pev;
+        int16_t year_prev; 
+        int8_t month_prev; 
+        int8_t day_prev; 
+        int8_t dow_prev; 
+        int8_t hour_prev; 
+        int8_t minute_prev; 
+
+        bool RedrawAlarm;
+        bool Alarm_Enable;
+        uint8_t AlarmDow;
+        uint8_t AlarmHour;
+        uint8_t AlarmMinute;
+            
+        TFT_eSPI* _lcd;
+
+ };
+ #endif
