87a8ca984cd66a1fd9a7fc1bfccfe97dc2171ff4
calm4git <35043090+calm4git@users.noreply.github.com>
2020-05-26 23:04:03 +0200

    Demo for Alarmmenu added

--- Firmware/src/Firmware.ino
+++ Firmware/src/Firmware.ino
@@ -1,5 +1,6 @@
 #include "SPI.h"
 #include "TFT_eSPI.h"
+#include "Free_Fonts.h"
 #include "src/Faces/Faces.h"
 #include "src/Faces/layout1/layout1.h"
 #include "src/Faces/layout2/layout2.h"
@@ -20,13 +21,9 @@ Faces* Face[] = {&L1, &L2, &L3 };
 static uint8_t F=0;
 tmElements_t tm;
 time_t utc_now;
-void setup() {
 
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  lcd.init();
-  lcd.setRotation(1);
-  lcd.fillScreen(TFT_BLACK);
+void setup_clock( void ){
+
   F=2;
   Face[F]->ForceScreenRefresh();
   tm.Hour=23;
@@ -35,8 +32,9 @@ void setup() {
   tm.Year=(2020-1970);
   utc_now = makeTime(tm);
   Face[F]->UpdateTime(utc_now);
-  Alarms.EnableAlarms();
+  
   Alarm::Alarmtime_t At;
+  Alarms.EnableAlarms();
   
   At.Day=0;
   At.IsRinging=0;
@@ -91,40 +89,224 @@ void setup() {
   At.Enabled = 1;
 
   Alarms.SetAlarm(2,At);
+}
+
+void RenderAlarmMenuEntry( uint32_t offset, bool selected,  Alarm::Alarmtime_t At  ){
+  uint8_t active_days=0;
+  
+  if(selected == true ){
+    lcd.fillRect(0,40+offset,300,40,lcd.color565( 92,96,102 ) );
+  } else {
+     lcd.fillRect(0,40+offset,300,40,TFT_WHITE );
+  }
+  lcd.drawRect(0,40+offset,300,40,lcd.color565( 112,116,122 ));
+  lcd.setTextColor(TFT_BLACK,TFT_BLACK);
+  lcd.setFreeFont(FSB12); 
+  lcd.setCursor(0,65+offset);
+  
+  if(selected == true ){
+    lcd.print(" >");
+  }
+  
+  lcd.setCursor(26,70+offset);
+  if(false == At.Enabled){
+    lcd.print("Alarm disabled");
+  } else {
+    if(true == At.UseDate){
+        if(At.Hour<10){
+          lcd.print("0");
+        } 
+        lcd.print(At.Hour);
+        lcd.print(":");
+        if(At.Minute<10){
+          lcd.print("0");
+        }
+        lcd.print(At.Minute);
+        lcd.setFreeFont(FSB9); 
+        lcd.setCursor(120,47+offset);
+        lcd.print(At.Day);
+        lcd.print(".");
+        lcd.print(At.Month);
+        lcd.print(".");
+        lcd.print(At.Year);
+
+    } else {
+        lcd.setCursor(30,67+offset);
+        if(At.Hour<10){
+          lcd.print("0");
+        } 
+        lcd.print(At.Hour);
+        lcd.print(":");
+        if(At.Minute<10){
+          lcd.print("0");
+        }
+        lcd.print(At.Minute);
+        lcd.setFreeFont(FSB9); 
+        lcd.setCursor(120,57+offset);
+        //We generate the Dow entry...
+        if(At.Monday>0){
+          lcd.print("Mo");
+          active_days++;
+          
+          if(active_days==5){
+             lcd.setCursor(120,74+offset);
+          }
+        }
+
+        if(At.Tursday>0){
+          if(active_days>0){
+            lcd.print(", ");
+          }
+          
+          lcd.print("Tu");
+          active_days++;
+          
+          if(active_days==5){
+             lcd.setCursor(120,74+offset);
+          }
+
+        }
+        
+        if(At.Wednesday>0){
+          if(active_days>0){
+            lcd.print(", ");
+          }
+          
+          lcd.print("We");
+          active_days++;
+
+          if(active_days==5){
+             lcd.setCursor(120,74+offset);
+          }
+        }
+
+        if(At.Thuseday>0){
+          if(active_days>0){
+            lcd.print(", ");
+          }
+          
+          lcd.print("Th");
+          active_days++;
+
+          if(active_days==5){
+              lcd.setCursor(120,74+offset);
+          }
+        }
+
+        if(At.Friday>0){
+          if(active_days>0){
+            lcd.print(", ");
+          }
+          
+          lcd.print("Fr");
+          active_days++;
+
+          if(active_days==5){
+              lcd.setCursor(120,74+offset);
+          }
+        }
+
+       
+        
+        if(At.Saturday > 0){
+          if(active_days>0){
+            lcd.print(", ");
+          }
+          
+          lcd.print("Sa");
+          active_days++;
+
+          if(active_days==5){
+              lcd.setCursor(120,74+offset);
+          }
+        }
+        if(At.Sunday > 0){
+          if(active_days>0){
+            lcd.print(", ");
+          }
+          
+          lcd.print("Su");
+          active_days++;
+
+          if(active_days==5){
+              lcd.setCursor(120,74+offset);
+          }
+        }
+        
+        lcd.setCursor(230,74+offset);
+        
+        if(true == At.OneShot){
+          lcd.print("Once");
+        }
+    }
+  }
+}
+
+void TestRenderMenuAlarms( uint8_t selected_idx){
+  Alarm::Alarmtime_t At ;
+  /* Draw Headline */
+  lcd.fillRect(0,0,320,40,lcd.color565( 45,47,50 ) );
+  
+  lcd.setTextColor(lcd.color565( 112,116,122 ),TFT_BLACK);
+  lcd.setFreeFont(FSB18); 
+  lcd.setCursor(160- ( lcd.textWidth("Alarms") / 2) ,30);
+  lcd.print("Alarms");
+  //Draw scrolling element
+  lcd.fillRect(302,43,15,196,TFT_WHITE);
+  lcd.drawRect(300,40,20,200,lcd.color565( 112,116,122 ));
+  lcd.drawRect(301,41,18,198,lcd.color565( 112,116,122 ));
+  lcd.drawRect(302,42,16,196,lcd.color565( 112,116,122 ));
+  //Next is to draw the elements as far as possibel....
+
+  
+  //We calcualte the range we need to draw or like to draw
+  //We can display 5 Elements 
+  uint8_t startidx=0;
+  if(selected_idx>4){
+    startidx = selected_idx -4 ;
+  }
+
+  //We set the scrollbar accordingly
+  lcd.fillRect(305,45 +(196/8)*selected_idx ,10,(196/8)-2 , TFT_DARKGREY);
+
+  for(uint8_t i=startidx;i<8;i++){
+    if(true == Alarms.GetAlarm(i,&At) ){
+      RenderAlarmMenuEntry( (40*(i-startidx))  ,( i==selected_idx ), At );
+    } 
+  }
+ 
+
+
+}
+
+
+
+
+void setup() {
+
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  lcd.init();
+  lcd.setRotation(1);
+  lcd.fillScreen(TFT_BLACK);
+  
+  setup_clock();
+
   
   
 }
 
 
-//The Clock will use utc timesamps 
-void loop() {
- 
-  static uint32_t ticks =0;
-  static uint32_t ft=0;
-   
-  
-  
-  delay(2);
-  //increase time and let the display draw
-  utc_now+=20;
-  ft++;
+void UpdateClock( time_t utc_now ){
   Alarms.CheckAlarms(utc_now);
-  if( ft> 340*60 ){
+  if( 0 == ( utc_now % 60 )  ){
       F++;
-      ft=0;
       if(F >=   3   ){
         F=0;
       }
       Face[F]->ForceScreenRefresh();
-      Face[F]->UpdateTime(utc_now);
   }
-
-  ticks++;
-  
-  if(true==Alarms.CheckAlarms(utc_now)){
-    //We need to ring 
-    Serial.printf("Ring Ring @ %u \n\r",utc_now );
-  }
+  Face[F]->UpdateTime(utc_now);
   Alarm* Al_Ptr=NULL;
   if(true == Alarms.GetNextAlarm(&Al_Ptr,utc_now)){
     time_t altime = Al_Ptr->GetNextAlarmTime(utc_now);
@@ -133,7 +315,32 @@ void loop() {
     Face[F]->UpdateAlarmInfo(false,0);
   }
   
-  Face[F]->UpdateTime(utc_now);
+  if(true==Alarms.CheckAlarms(utc_now)){
+    //We need to ring 
+    Serial.printf("Ring Ring @ %u \n\r",utc_now );
+    //Signal that we do a kind of ring or play a mp3....
+
+  }
+}
+
+
+
+
+//The Clock will use utc timesamps 
+void loop() {
+  //We run only once a second to update the alarms and display if the second updated....
+  //increase time and let the display draw
+  for(uint8_t i=0;i<7;i++){
+    TestRenderMenuAlarms( i );
+    delay(500);
+  }
+
+  for(uint8_t i=0;i<7;i++){
+    TestRenderMenuAlarms( 7-i );
+    delay(500);
+  }
+ 
+
   
   
 
